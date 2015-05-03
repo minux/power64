adc0ccea9bac6d8d2846cb6972bd45c211e7a94c
Shenghou Ma <minux@golang.org>
2015-05-03 15:47:05 -0400

    power64asm: add package doc comment
    Signed-off-by: Shenghou Ma <minux@golang.org>

---
+++ power64asm/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2014 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package power64asm implements decoding of 64-bit Power machine code.
+package power64asm
